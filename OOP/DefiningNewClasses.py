a457de43319f17207c87a71218337d757e4cbe89
KalebeSantana <santana.abrantes01@gmail.com>
2022-09-03 21:31:56 -0300

    OOP

---
+++ OOP/DefiningNewClasses.py
@@ -0,0 +1,13 @@
+class Apple:
+    color = ""
+    flavor = ""
+
+jonagold = Apple()
+jonagold.color = "red"
+jonagold.flavor = "sweet"
+print('Color: {}\nFlavor: {}'.format(jonagold.color, jonagold.flavor))
+
+golden = Apple()
+golden.color = "Yellow"
+golden.flavor = "Soft"
+print('Color: {}\nFlavor: {}'.format(golden.color, golden.flavor))
